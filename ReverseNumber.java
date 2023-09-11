3184cf8a3bd1e491c8f317682f7800607fa5a698
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-11 13:28:34 +0530

    second commit

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class ReverseNumber {
+    private static final Scanner scanner = new Scanner(System.in);
     
     public static void main(String[] args) {
+        System.out.print("Enter a number to Reverse: ");
+        int Num = scanner.nextInt();
+        scanner.nextLine();
+
         
     }
 }
