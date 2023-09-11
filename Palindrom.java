eae87d9526927934123616b46e8e09509f88d720
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-11 15:13:16 +0530

    Second commit

--- Palindrom.java
+++ Palindrom.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Palindrom {
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+        System.out.print("Enter the Number to Find the Parindrom:");
+
         
     }
 }
