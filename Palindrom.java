3a11297bc974246f49d56a1dc8af5f6ca08476ad
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-11 15:15:33 +0530

    Third commit

--- Palindrom.java
+++ Palindrom.java
@@ -5,6 +5,10 @@ public class Palindrom {
 
     public static void main(String[] args) {
         System.out.print("Enter the Number to Find the Parindrom:");
+        int Num = scanner.nextInt();
+        scanner.nextLine();
+
+        int Reverse = 0;
 
         
     }
