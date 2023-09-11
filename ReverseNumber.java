a8985e2716deb3a2ad5c2ac2b22470d1b4ed79d7
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-11 14:47:28 +0530

    Last commit

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -4,9 +4,20 @@ public class ReverseNumber {
     private static final Scanner scanner = new Scanner(System.in);
     
     public static void main(String[] args) {
+
         System.out.print("Enter a number to Reverse: ");
         int Num = scanner.nextInt();
         scanner.nextLine();
+
+        int Reverse = 0;
+        while(Num !=0){
+            int Mod = Num%10;
+            Reverse = Reverse*10 + Mod;
+            Num = Num/10;
+        }
+        System.out.println(Reverse);
+        
+        
 
         
     }
