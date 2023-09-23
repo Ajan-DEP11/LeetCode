1d9a2d0acb268d0caf641614645e51a7f3b4a892
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-23 19:14:47 +0530

    second commit

--- PlusOne1.java
+++ PlusOne1.java
@@ -1,5 +1,24 @@
+import java.util.Arrays;
+
 public class PlusOne1 {
     public static void main(String[] args) {
+        
+        int[] num = {1,2,3,9};
+        
+        for(int i= num.length-1; i>=0;i--){
+
+            if(num[i] < 9){
+                num[i]++;
+                return;
+            }
+            num[i] =0;
+
+
+        }
+        num = new int[num.length +1];
+        num[0] = 1;
+        return;
+        
         
     }
 }
