a86e00566d13368f2663e7b46999f1b59edbd6b1
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-27 20:35:59 +0530

    Add pattern

--- SqureStarPattern.java
+++ SqureStarPattern.java
@@ -3,5 +3,14 @@ public class SqureStarPattern {
         for(int i=0;i<5;i++){
             System.out.println("* * * * *");
         }
+
+        System.out.println("=============================");
+
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
